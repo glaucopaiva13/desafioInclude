62b7596b9e52368252dd794c08cbcbbb0a07ba67
unknown <glaucopaiva013@gmail.com>
2024-11-17 17:05:39 -0300

    Ajeitei pastas

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 function toggle() {
   const html = document.documentElement;
-  const audio = new Audio("./tv.wav");
+  const audio = new Audio("./assets/tv.wav");
   html.classList.toggle('on')
   audio.play()
 }
